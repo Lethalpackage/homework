6c2b9b3c0fa010b699193d799fb2a00f5fdf87eb
kirill <kirill1997.56@mail.ru>
2021-10-02 21:44:59 +0500

    data structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-//        ListH<String> list = new ListH<>();
-//        list.add("Fisting");
-//        list.add("anal");
-//        list.add("gfgf");
-//
-//        for(int i =0; i< list.length(); i++){
-//            try {
-//                System.out.println(list.get(i));
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        System.out.println("gfdgd");
+       ListH<String> list = new ListH<>();
+        list.add("Fisting");
+        list.add("anal");
+        list.add("gfgf");
 
-        ArrayList list = new ArrayList<Magaz>();
-//        list.add( new Magaz("Sevas", 5));
-//        list.add(new Magaz());
-//        list.add(new Magaz("dsws", 3));
+        for(int i =0; i< list.length(); i++){
+            try {
+                System.out.println(list.get(i));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("gfdgd");
 
-        Magaz hgd = new Magaz();
-        hgd.name = "tyhgd";
+
 
     }
 
